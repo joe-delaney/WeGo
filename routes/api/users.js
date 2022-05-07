bf9c23d33d6a2143188ce87e2f3ed8baafa94c21
joe-delaney <joseph.delaney546@gmail.com>
2022-05-07 16:59:56 -0400

    user token is returned upon a successful login

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 //User sign up backend route
 router.post('/signup', (req, res) => {
@@ -52,9 +54,17 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if (isMatch) {
-                        res.json({ msg: 'Success' });
+                        const payload = { id: user.id, handle: user.handle };
+
+                        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                            res.json({
+                                success: true,
+                                token: "Bearer " + token
+                            });
+                        });
                     } else {
-                        return res.status(400).json({ password: 'Incorrect password' });
+                        errors.password = "Incorrect password";
+                        return res.status(400).json(errors);
                     }
                 })
         })
