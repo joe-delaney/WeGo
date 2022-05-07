19a1a78c9c6d04b8b66fa33e687bcbb88e9b8f06
joe-delaney <joseph.delaney546@gmail.com>
2022-05-07 16:35:43 -0400

    finished express setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -10,8 +11,11 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users);
+
 app.get("/", (req, res) => res.send("Hello World"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-app.use("/api/users", users);
