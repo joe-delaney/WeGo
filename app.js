8f87aa6a2c1e236a47545edaa75b3c8353ce1c59
joe-delaney <joseph.delaney546@gmail.com>
2022-05-07 16:34:17 -0400

    added user test route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
 
 const app = express();
 
@@ -12,3 +13,5 @@ mongoose
 app.get("/", (req, res) => res.send("Hello World"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.use("/api/users", users);
