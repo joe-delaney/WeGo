830c5f9b22e1fa111e59f072f139a1e49cf17455
JaniceShih <janice.hc.shih@gmail.com>
2022-05-08 12:52:30 -0400

    Create index.css as main.css and navbar.css

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -10,24 +10,27 @@ class NavBar extends React.Component {
 
     render() {
         return (
-          <div className='NavBar'>
+          <div className='nav'>
               <div>
-                  <span style={{ color: "#0071bc", fontSize: 'x-large'}}>
+                  <span style={{ color: "#0071bc", fontSize: 'x-large', fontWeight: '700'}}>
                      W   
                   </span>
                   <span style={{ color: "#42b2fc"}}>
                      e  
                   </span>
-                  <span style={{ color: "#3f89bb", fontSize: 'x-large'}}>
+                  <span style={{ color: "#3f89bb", fontSize: 'x-large', fontWeight: '700'}}>
                      G 
                   </span>
                   <span style={{ color: "#08c3fc"}}>
                      o  
                   </span>
               </div>
-              <div>
-                <Link to={'/signup'} >Signup</Link>
-                <Link to={'/login'}>Login</Link>
+              <div className='nav__right'>
+                <ul class="list nav__list">
+                  <li class="nav__item"><Link to={'/signup'} >Signup</Link></li>
+                  <li class="nav__item"><Link to={'/login'}>Login</Link></li>
+                </ul>
+                
               </div>
                 
           </div>
