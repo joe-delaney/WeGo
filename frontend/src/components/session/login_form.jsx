0ea38fdd0e93e1445414262f6b9dbbcbd3022499
joe-delaney <joseph.delaney546@gmail.com>
2022-05-08 09:52:24 -0400

    added in session form components and containers

---
+++ frontend/src/components/session/login_form.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+export default class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: ""
+        }
+
+        this.handleInput = this.handleInput.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
+    handleInput(type) {
+        return e => {
+            this.setState({
+                [type]: e.target.value
+            })
+        }
+    }
+
+    handleLogin(type) {
+        //Login the user
+        this.props.login(this.state);
+
+        //Close the login modal
+        this.toggleModal();
+    }
+
+    toggleModal() {
+        //This will be used to close the modal once implemented
+    }
+
+    render() {
+        return (
+            <form>
+                <h1>Log in to your account!</h1>
+                <input 
+                    type="text" 
+                    placeholder="email" 
+                    value={this.state.email} 
+                    onChange={this.handleInput("email")}/>
+                <input 
+                    type="password" 
+                    placeholder="password" 
+                    value={this.state.password} 
+                    onChange={this.handleInput("password")}/>
+                <input 
+                    type="submit" 
+                    value="Login" 
+                    onClick={this.handleLogin}/>
+            </form>
+        )
+    }
+}
