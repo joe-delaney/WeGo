a63774ccad6eaf96f42006291f44142b1384da41
JaniceShih <janice.hc.shih@gmail.com>
2022-05-09 10:27:45 -0400

    create profile component

---
+++ frontend/src/components/profile/profile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './profile.css'
+
+
+
+class Profile extends React.Component {
+    constructor(props) {
+      super(props);
+
+    }
+
+    render() {
+        return(
+            <div>Profile</div>
+        )
+    }
+}
+
+export default Profile;
+
