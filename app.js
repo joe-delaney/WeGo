229f99f5d9bb27d59d479d9bb2b4bcd13659c491
joe-delaney <joseph.delaney546@gmail.com>
2022-05-09 11:46:32 -0400

    heroku setup

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const users = require("./routes/api/users");
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
 const app = express();
 
 // MongoDB
