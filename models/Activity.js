1fad49d78f22cd52a9f8a6d160b3018012d88002
hisrael1 <harrisonisrael23@gmail.com>
2022-05-09 18:14:51 -0400

    activityModel

---
+++ models/Activity.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const { arrayBuffer } = require('stream/consumers');
+const Schema = mongoose.Schema;
+
+
+// How much info above and beyond the id's will we need to embed?
+// look up n+1 query
+
+const ActivitySchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        index: true
+    },
+    time: {
+        type: Date,
+        required: true
+    },
+    host: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    requestedAttendees: [{
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    }],
+    approvedAttendees: [{
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    }],
+    tags: [{
+        type: String,
+        require: false
+    }],
+    location: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    capacity: {
+        type: Number,
+        required: true
+    },
+    closed: {
+        type: Boolean,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+
+module.exports = User = mongoose.model('Activity', ActivitySchema);
