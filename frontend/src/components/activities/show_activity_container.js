f26aab0c9f098cb8d534f06c32ed3000315c9c53
joe-delaney <joseph.delaney546@gmail.com>
2022-05-10 17:51:00 -0400

    started activity show page

---
+++ frontend/src/components/activities/show_activity_container.js
@@ -0,0 +1,13 @@
+import { connect } from "react-redux";
+import ShowActivity from "./show_activity"
+
+const mapStateToProps = (state, ownProps) => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+const ShowActivityContainer = connect(mapStateToProps, mapDispatchToProps)(ShowActivity);
+export default ShowActivityContainer;
