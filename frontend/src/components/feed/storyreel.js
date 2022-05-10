d2e666214a067314d71b8c8ad6507c2635aa5a03
joe-delaney <joseph.delaney546@gmail.com>
2022-05-10 14:15:19 -0400

    fixed signup form

--- frontend/src/components/feed/storyreel.js
+++ frontend/src/components/feed/storyreel.js
@@ -16,40 +16,11 @@ export default class StoryReel extends React.Component {
         let activities = Array.isArray(this.props.activities) ? this.props.activities : [];
         return (
             <div className='storyreel'>
-                {activities.map((activity, idx) => (
-                    <Story
-                        key={idx}
-                        image="https://a.cdn-hotels.com/gdcs/production88/d1000/f1fd2bd5-e90f-48fa-85d1-840e2c4ace3b.jpg"
-                        profileSrc=""
-                        title={activity.title}
-                    />
-                ))}
-            </div>
-        )
-    }
-
-    render() {
-        
-
-
-
-        return (
-            <div className='storyreel'>
-                {/* <Story 
-            image=""
-            profileSrc="Add Story"
-            title="a/A Student"
-        /> */}
                 <Story
                     image="https://psychology-spot.com/wp-content/uploads/2019/10/new-music.jpg"
                     profileSrc=""
                     title="Music"
                 />
-                {/* <Story 
-            image="https://smashgadget.com/wp-content/uploads/2021/07/laptop-for-movies.jpg"
-            profileSrc=""
-            title="Movie"
-        /> */}
                 <Story
                     image="https://chriskresser.com/wp-content/uploads/iStock-951861300-martin-dm.jpg"
                     profileSrc=""
@@ -60,11 +31,39 @@ export default class StoryReel extends React.Component {
                     profileSrc=""
                     title="Shopping"
                 />
-                {this.getActivities()}
-
+                {activities.map((activity, idx) => (
+                    <Story
+                        key={idx}
+                        image="https://a.cdn-hotels.com/gdcs/production88/d1000/f1fd2bd5-e90f-48fa-85d1-840e2c4ace3b.jpg"
+                        profileSrc=""
+                        title={activity.title}
+                    />
+                ))}
+                <Story
+                    image="https://img.icons8.com/material/344/new--v1.png"
+                    profileSrc=""
+                    title="Create new activity"
+                    addNew={true}
+                />
             </div>
         )
     }
 
+    render() {
+        return this.getActivities();
+    }
+
+            //     <div className='storyreel'>
+        //         {/* <Story 
+        //     image=""
+        //     profileSrc="Add Story"
+        //     title="a/A Student"
+        // /> */}
+        //         {/* <Story 
+        //     image="https://smashgadget.com/wp-content/uploads/2021/07/laptop-for-movies.jpg"
+        //     profileSrc=""
+        //     title="Movie"
+        // /> */}
+        //     </div>
 
 }
