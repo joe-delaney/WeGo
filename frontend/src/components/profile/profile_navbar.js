2a9641b29bc741007c09e230f060e8d64a912306
joe-delaney <joseph.delaney546@gmail.com>
2022-05-11 17:16:02 -0400

    fixed some profile page styling

--- frontend/src/components/profile/profile_navbar.js
+++ frontend/src/components/profile/profile_navbar.js
@@ -33,23 +33,15 @@ class ProfileFeed extends React.Component {
                     </div>
                    
                     <div className='profile__userinfo--heading'>
-                            <h1>{fullName}</h1>
-                            <h2>{pronouns}</h2>
-                            <p>{age}</p>
+                            <h1 className='profile-name'>{fullName}</h1>
+                            <h2 className='profile-pronouns'>{pronouns}</h2>
+                            <p className='profile-age'>{age}</p>
                             
                     </div>  
                    
                 </div>
             </div>
-
- 
-            {/* <div className='profile__navbar'>                    
-                <div className="profile__option"> About </div>
-            </div>  */}
          </>
-
-            
-
         )
     }
 }
