62e0218432b4476243e5b41fa6e3fa6dc2b8c22c
joe-delaney <joseph.delaney546@gmail.com>
2022-05-12 15:39:41 -0400

    minor changes

--- frontend/src/components/feature/feature.js
+++ frontend/src/components/feature/feature.js
@@ -36,20 +36,6 @@ export default class Feature extends React.Component {
                         <img className="feature__image" src="https://media4.giphy.com/media/MvLWScDJ6TPvp0ntC6/giphy.gif?cid=790b76112fea0c53879adcf9d6c46e3e9efaf8d6b701f971&rid=giphy.gif&ct=g" alt="" />
                     </div>
                 </div>
-                {/* <div className='feature__block feature'>
-                    <div className="feature__content">
-                        <h3 className="feature__heading">Personalized Search</h3>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-                            obcaecati vel ad unde est illo at. Labore excepturi officia dolores!
-                        </p>
-
-                    </div>
-                    <div>
-                        <img className="feature__image" src="https://img.forconstructionpros.com/files/base/acbm/fcp/image/2021/04/soil_connect_logo.607ed736705d0.png?auto=format%2Ccompress&fit=crop&h=288&rect=161%2C96%2C874%2C491&w=512" alt="" />
-                    </div>
-
-                </div> */}
             </>
         )
     }
