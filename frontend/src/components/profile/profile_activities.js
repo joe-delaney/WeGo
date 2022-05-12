17d2d4a647e90a54381c9692cd5a47b5db6fb5d3
joe-delaney <joseph.delaney546@gmail.com>
2022-05-12 18:01:48 -0400

    finished styling user profile activities

--- frontend/src/components/profile/profile_activities.js
+++ frontend/src/components/profile/profile_activities.js
@@ -13,43 +13,12 @@ class ProfileActivities extends React.Component {
           <>
               {allActivities.map((activity, idx) => (
                 <IndexItemActivity
-                  image="https://psychology-spot.com/wp-content/uploads/2019/10/new-music.jpg"
                   key={idx}
                   activity={activity}
                   renderClass="profile__activtiy--col"
                   openModal={this.props.openModal}
                 />
               ))}
-              {/* <IndexItemActivity 
-                  image="https://psychology-spot.com/wp-content/uploads/2019/10/new-music.jpg"
-                  titile ="Shopping"
-                  date = "05/20/202"
-                  Location =" NYC"  
-                  renderClass="profile__activtiy"                
-                />
-                <IndexItemActivity 
-                  image="https://chriskresser.com/wp-content/uploads/iStock-951861300-martin-dm.jpg"
-                  titile ="Shopping"
-                  date = "05/20/202"
-                  Location =" NYC"
-                  renderClass="profile__activtiy"  
-                />
-                  <IndexItemActivity 
-                  image="https://d3hne3c382ip58.cloudfront.net/files/uploads/bookmundi/resized/cmsfeatured/trekking-in-japan-1560935848-785X440.jpg"
-                  titile ="Shopping"
-                  date = "05/20/202"
-                  Location =" NYC"
-                  renderClass="profile__activtiy"  
-                />
-
-                <IndexItemActivity 
-                  image="https://a.cdn-hotels.com/gdcs/production88/d1000/f1fd2bd5-e90f-48fa-85d1-840e2c4ace3b.jpg"
-                  titile ="Shopping"
-                  date = "05/20/202"
-                  Location =" NYC"
-                  renderClass="profile__activtiy"  
-                /> */}
-          
         </>
                      
         )
