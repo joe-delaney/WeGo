08a493e8b366b446bec6bc87a0d45492ca6e94a7
joe-delaney <joseph.delaney546@gmail.com>
2022-05-12 20:28:38 -0400

    styling messages

--- frontend/src/components/messages/messages.js
+++ frontend/src/components/messages/messages.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ChatgroupContainer } from './chatgroup_container';
 import { ConversationModalContainer } from './conversation_modal_container';
+import "./message.css";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 export class Messages extends React.Component {
   constructor(props) {
@@ -22,9 +24,17 @@ export class Messages extends React.Component {
   render() {
     return (
       (!this.props.user) ? null :
-      <div className='outermost_messenger_container'>
-
+      <div className='messages-modal-container'>
         <div className='conversations-container'>
+          <div className="conversations-container-nav">
+              <div className='conversations-container-nav-left'>
+                <img src={this.props.user.profilePhotoPath} className='conversations-container-img'></img>
+                <strong className='conversations-container-nav-header'>Messages</strong>
+              </div>
+              <div className='conversations-container-nav-left'>
+                <KeyboardArrowDownIcon sx={{ fontSize: 25, color: '#000' }}/>
+              </div>
+          </div>
           <h1 className='messenger header'> Messages <span>Stand in for unread messages symbol</span> </h1>
           <button onClick={this.conversationToggle} />
           <div className='sub-conversation-container'>
