7810795ddb7be1f451514096e5c43a8cd3bc7c71
joe-delaney <joseph.delaney546@gmail.com>
2022-05-13 00:13:11 -0400

    created message item component

---
+++ frontend/src/components/messages/message.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default class Message extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        //replace with message info
+        let messageImg = "/api/images/667fc4dc5c22753e3a5b1a6bdd888ee2"
+        let messageAuthor = "Joe Delaney";
+        let messageText = "These messages look pretty good!"
+
+        return (
+            <li>
+                <img className="message-img" src={messageImg}></img>
+                <div className="message-list-item-right">
+                    <span className="message-list-item-name">{messageAuthor}</span>
+                    <span className="message-list-item-text">{messageText}</span>
+                </div>
+            </li>
+        )
+    }
+}
