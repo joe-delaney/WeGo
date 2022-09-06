f463c1493a0e4e94b1e3746c10cfc7904c421a3b
Nicholas <nick.angleton@gmail.com>
2022-09-06 08:02:01 -0400

    comment out socket code

--- frontend/src/components/socket.js
+++ frontend/src/components/socket.js
@@ -1,5 +1,5 @@
-import React from "react";
-import io from "socket.io-client";
+// import React from "react";
+// import io from "socket.io-client";
 
-export const socket = io();
-export const SocketContext = React.createContext(socket);
+// export const socket = io();
+// export const SocketContext = React.createContext(socket);
